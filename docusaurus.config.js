838f19f6ab2ea0738db4dac88b07cb516996f54b
zeevava <68116900+zeevava@users.noreply.github.com>
2026-02-14 19:49:37 +0700

    Update Docusaurus config for new site URL and base path

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,10 +20,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://docs.ff.studio',
+  url: 'https://zeevava.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/ff-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
